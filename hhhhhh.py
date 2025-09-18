cd9e5b29391b4ee361f766748640d6b34639fbb3
hemantsumant2166-creator <hemantsumant2166@gmail.com>
2025-09-17 23:18:12 -0700

    Add files via upload

---
+++ hhhhhh.py
@@ -0,0 +1,37 @@
+'''def saus(a,b):
+    print(a+b)
+saus (5,8)'''
+
+'''def detail(name,age,city):
+    print(name,age,city)
+detail("hemant",19,"gurgaon")'''
+
+'''def saud(num):
+    if num %2==0:
+      print("even")
+    else:
+      print("odd")
+saud(7)'''
+
+       
+def calculator(a,b,operator):
+    if operator =="+":
+     print(a+b)
+    elif operator =="-":
+     print(a-b)
+    elif operator =="*":
+     print(a*b)
+    elif operator =="/":
+     print(a/b)
+    else:
+     print("saud")
+calculator(98,65,"8")
+
+    
+
+        
+
+    
+
+
+
